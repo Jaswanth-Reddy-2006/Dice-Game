5ae363c2a910a628c7c4a72c65aa5f4b33ebc9ab
Jaswanth-Reddy-2006 <jaswanthre9@gmail.com>
2025-06-21 20:11:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var image1 = "/images/dice" + randomNumber1 + ".png";
+var image1 = "dice" + randomNumber1 + ".png";
 document.querySelector(".img1").setAttribute("src",image1);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var image2 = "/images/dice" + randomNumber2 + ".png";
+var image2 = "dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src",image2);
 
 
